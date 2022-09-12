98ccc38a31f051194438c155ee30cacf5498ba1a
Hardin Santoso <hardinzuhdisantoso@gmail.com>
2022-09-12 07:31:55 +0700

    Initial Adding of Incubation service page

---
+++ components/Incubation/Banner.tsx
@@ -0,0 +1,125 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from "react";
+import { useRouter } from "next/router";
+import {
+  HStack,
+  Image,
+  Flex,
+  Text,
+  Stack,
+  Container,
+  Box,
+  Center,
+  chakra,
+  IconButton,
+} from "@chakra-ui/react";
+import { ImageTransition } from "../ImageTransition";
+import { useStore } from "../../contexts/store";
+import { FaArrowRight, FaTwitter } from "react-icons/fa";
+
+export default function Banner() {
+  const router = useRouter();
+  const { state, dispatch } = useStore();
+  return (
+    <Flex
+      width="100%"
+      id="heroComponent"
+      textAlign="center"
+      position="relative"
+      alignItems="center"
+      flexDirection="column"
+      height={{ base: "25em", md: "55vh", lg: "60vh" }}
+    >
+      <Container
+        position={"relative"}
+        mt={{ base: "10vh", md: "12vh", lg: "15vh" }}
+        zIndex={"3"}
+        maxW="container.lg"
+      >
+        <Stack alignItems={"center"}>
+          <Text
+            fontFamily="PilatExtended-Bold"
+            fontSize={{ base: "16px", md: "36px" }}
+            lineHeight={{ base: "30px", md: "1em", lg: "1.1em" }}
+            letterSpacing={{ base: "0.1em" }}
+            textShadow="0px 10px 10px rgba(9, 2, 90, 0.73)"
+            color={"white"}
+            mb={"50px"}
+          >
+           We Help Take Your Inspiring Projects <chakra.span color={"#00C1FF"}>to The Next Level</chakra.span>
+          </Text>
+          <Box
+            background={" rgba(0, 0, 19, 0.78)"}
+            border={"3px solid rgba(15, 177, 245, 0.28)"}
+            width={"1100px"}
+            height={"250px"}
+            rounded="10px"
+            paddingTop={"3%"}
+          >
+            <Text
+              fontFamily="PilatExtended-Light"
+              fontSize={{ base: "12px", md: "20px" }}
+              fontWeight={"500"}
+              lineHeight={{ base: "30px", md: "1em", lg: "1.1em" }}
+              letterSpacing={{ base: "0.1em" }}
+              textShadow="0px 10px 10px rgba(9, 2, 90, 0.73)"
+            >
+              Incubating the Best Web3 and Web2 Projects
+            </Text>
+            <Text mt={"12px"}>
+              WeFund is the leading Web3 and Web2 incubator and founder
+              community. We accept and incubate Web2 projects looking to bridge <br/>
+              blockchain or existing Web3 technologies with real-world
+              application and utility.
+            </Text>
+
+            <HStack w="100%" justify={"center"} mt="48px" spacing="20px">
+              <ImageTransition
+                unitid={"buywfd"}
+                border1="#23A4EC"
+                background1="#06142D"
+                border2="#06142D"
+                background2="#23A4EC"
+                border3="linear-gradient(180deg, #00A3FF 0%, #0047FF 100%)"
+                background3="linear-gradient(180deg, #171347 0%, #171347 100%)"
+                selected={false}
+                width={{ base: "188px", md: "248px", lg: "402px" }}
+                height={{ base: "32px", md: "40px", lg: "40px" }}
+                rounded={"33px"}
+                onClick={() => router.push("/applyproject")}
+              >
+                <Text
+                  w="100%"
+                  fontSize={{
+                    base: "14px",
+                    sm: "15px",
+                    md: "15px",
+                    lg: "15px",
+                  }}
+                  fontFamily={"PilatExtended-Bold"}
+                  fontWeight={"800"}
+                  marginLeft={7}
+                  color="#002E87"
+                  _hover={{ color: "#FFFFFF" }}
+                  transition={"all 1s"}
+                >
+                  Apply to be Project on WeFund
+                  <IconButton
+                    variant="unstyled"
+                    marginLeft={4}
+                    aria-label="Twitter"
+                    width={{ lg: "14px", base: "12px" }}
+                    icon={<FaArrowRight />}
+                  />
+                </Text>
+              </ImageTransition>
+            </HStack>
+          </Box>
+        </Stack>
+      </Container>
+      <Center position={"absolute"} top="52">
+        <Image src="/media/Home/Circular_BG.svg" />
+      </Center>
+    </Flex>
+  );
+}
---
+++ pages/incubation/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+
+import "react-multi-carousel/lib/styles.css";
+import Container from "../../components/Container";
+
+import theme from "../../theme";
+import Footer from "../../components/Footer";
+
+import Hero from "../../components/Launchpad/Hero";
+import Highlights from "../../components/Launchpad/Highlights";
+import UpcomingProject from "../../components/Launchpad/UpcomingProject";
+import OngoingProject from "../../components/Launchpad/OngoingProject";
+import CompletedProject from "../../components/Launchpad/CompletedProject";
+import KYCBanner from "../../components/Launchpad/KYCBanner";
+import Staking from "../../components/Launchpad/Staking";
+import CompletedSales from "../../components/Launchpad/CompletedSales";
+import ProjectStories from "../../components/Launchpad/ProjectStories";
+import PTabs from "../../components/Launchpad/ProjectTabs";
+import Banner from "../../components/Incubation/Banner";
+
+export default function Launchpad() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Container>
+        <Banner />
+        <Footer />
+      </Container>
+    </ChakraProvider>
+  );
+}
